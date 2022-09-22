b5e03e0827472bb6cc5bef0f4d51cddea949d20f
deligo21 <arrp1239@gmail.com>
2022-09-22 00:41:39 -0400

    instalacion de yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,24 @@
 import { crearArchivo } from './helpers/multiplicar.js';
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+const argv = yargs(hideBin(process.argv)).argv
+
 
 console.clear();
 
-//Manera primitiva
-const[,,arg3 = 'base=5'] = process.argv;
-const[,base = 5] = arg3.split('=');
+console.log(process.argv);
+console.log(argv);
 
-console.log(base);
+console.log('base: yargs', argv.base);
+
+//Manera primitiva
+// const[,,arg3 = 'base=5'] = process.argv;
+// const[,base = 5] = arg3.split('=');
+
+// console.log(base);
 
 // const base = 6;
 
-crearArchivo( base )
-    .then(nombreArchivo => console.log(nombreArchivo, ' ha sido creado'))
-    .catch(err => console.log(err));
+// crearArchivo( base )
+//     .then(nombreArchivo => console.log(nombreArchivo, ' ha sido creado'))
+//     .catch(err => console.log(err));
