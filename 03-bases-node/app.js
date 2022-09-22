e9f440d0ff749a0b432dda14a9e2d2731286a0c0
deligo21 <arrp1239@gmail.com>
2022-09-22 00:16:14 -0400

    package json

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,7 +2,13 @@ import { crearArchivo } from './helpers/multiplicar.js';
 
 console.clear();
 
-const base = 6;
+//Manera primitiva
+const[,,arg3 = 'base=5'] = process.argv;
+const[,base = 5] = arg3.split('=');
+
+console.log(base);
+
+// const base = 6;
 
 crearArchivo( base )
     .then(nombreArchivo => console.log(nombreArchivo, ' ha sido creado'))
